ff3c20f82e80ce0fe5845e061e109d9cae0d340b
Nikkelodeons <west.nicolea@gmail.com>
2025-08-10 02:26:32 -0400

    blah

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,5 @@
-// @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-
-import { template } from "./src/settings";
-
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind(), sitemap()],
-    site: template.website_url,
-    base: template.base,
+  site: 'https://nikkelodeons.github.io/steiner_lab_site/',  // update when you have a custom domain
 });
